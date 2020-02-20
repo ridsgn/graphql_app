b2371813e09bbb9e623f6d417dfb03d4884f1554
rizky <rizukifirumanshaa96@gmail.com>
2020-02-20 11:16:49 +0700

    Modifying schema
    Add Root query and dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,13 @@ const qraphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// dummy data
+var books = [
+	{ name: 'Name of the wind', genre: 'Fantasy', id: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
+
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	fields: () => ({
@@ -19,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLString } },
 			resolve(parent, args) {
 				// code to get data from db / other source
+				return _.find(books, { id: args.id });
 			}
 		}
 	}
