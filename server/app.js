54056bf2d85b021581736929e8a6ba808b135571
rizky <rizukifirumanshaa96@gmail.com>
2020-02-20 13:18:56 +0700

    Use mongodb for database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+// const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+
+const uri =
+	'mongodb+srv://rizuki96:linkinmeteora99@cluster0-4arkx.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+client.connect(err => {
+	const collection = client.db('test').collection('devices');
+	// perform actions on the collection object
+	client.close();
+});
 
 app.use(
 	'/graphql',
