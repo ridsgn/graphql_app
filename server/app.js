9e04fa3c36bd63554275ab90186bfed854892c2e
rizky <rizukifirumanshaa96@gmail.com>
2020-02-20 11:33:16 +0700

    Fix typo

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schemas');
+const schema = require('./schema/schema');
 
 const app = express();
 
